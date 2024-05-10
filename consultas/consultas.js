0672c1d2d8ea696d4d239cd864cf0c66f9bffd77
Romina <rominchuela@gmail.com>
2024-05-10 19:08:57 -0400

    limpieza main y consultas

--- consultas/consultas.js
+++ consultas/consultas.js
@@ -1,43 +1,31 @@
 // Conexion a base de datos
 const pool = require('./config/conexion.js');
 
-// ● / GET: Devuelve la aplicación cliente disponible en el apoyo de la prueba. 
-//no requiere consulta xq sirve el html nomas
 
-//  ● /usuario POST: Recibe los datos de un nuevo usuario y los almacena en PostgreSQL.
+//  agregar usuario
 async function agregarUsuario(nombre, balance) {
     console.log("Valores recibidos: ", nombre, balance);
     const result = await pool.query({
-        text: 'INSERT INTO usuarios (id, nombre, balance) VALUES (default, $1, $2) RETURNING *', //REVISAR LO DE DEFAULT
+        text: 'INSERT INTO usuarios (id, nombre, balance) VALUES (default, $1, $2) RETURNING *',
         values: [nombre, balance]
     })
-    console.log("Registro agregado: ", result.rows[0]); //result.rows[0] es la posicion 0 de un array o de un objeto?
-    //Respuesta de la funcion
+    console.log("Registro agregado: ", result.rows[0]);
     return result.rows[0];
-    // return "Registro agregado con exito"
 };
-// agregarUsuario('nombre1', 0);
 
-//   ● /usuarios GET: Devuelve todos los usuarios registrados con sus balances.
-
+//mostrar usuarios
 async function todos() {
     const result = await pool.query("SELECT * FROM usuarios order by id");
     return result.rows;
 }
 
-//    ● /usuario PUT: Recibe los datos modificados de un usuario registrado y los actualiza.
-
+//editar usuario
 async function editarUsuario(id, nombre, balance) {
-    const result = await pool.query("UPDATE usuarios SET nombre = $2, balance = $3 WHERE id = $1 RETURNING *", [id, nombre, balance]); //DA LO MISMO EL ORDEN EN QUE INGRESAN LOS PARAMETROS??
+    const result = await pool.query("UPDATE usuarios SET nombre = $2, balance = $3 WHERE id = $1 RETURNING *", [id, nombre, balance]);
     return result.rows[0];
 }
 
-// editarUsuario(1,'nombrw1',1)
-
-//     ● /usuario DELETE: Recibe el id de un usuario registrado y lo elimina .
-
-//AHORA PONER CASCADE
-
+//eliminar usuario ***FALTA CASCADE***
 const eliminarUsuario = async (id) => {
     const result = await pool.query(
         {
@@ -48,13 +36,10 @@ const eliminarUsuario = async (id) => {
     return result.rows;
 };
 
-//      ● /transferencia POST: Recibe los datos para realizar una nueva transferencia. Se debe ocupar una transacción SQL en la consulta a la base de datos.
-
-/////////// VER LO DE LA FECHA
-
+//proceso tranferencia
 async function transferencias(emisor, receptor, monto, fechaHoraActual) {
-    
-    
+
+
     try {
         //fecha
         const fechaHoraActual = (await pool.query("SELECT NOW()")).rows[0].now;
@@ -71,7 +56,9 @@ async function transferencias(emisor, receptor, monto, fechaHoraActual) {
 
         console.log('ids:', idEmisor, idReceptor)
 
+        //inicio transaccion
         await pool.query("BEGIN");
+        //quitar a emisor
         const resta = "UPDATE usuarios SET balance = balance - $1 WHERE id = $2 RETURNING *";
         const res1 = await pool.query(resta, [monto, idEmisor]);
         if (res1) {
@@ -80,7 +67,7 @@ async function transferencias(emisor, receptor, monto, fechaHoraActual) {
         } else {
             console.error("Error al ejecutar la consulta SQL 1");
         }
-
+        //entregar a receptor
         const suma = "UPDATE usuarios SET balance = balance + $1 WHERE id = $2 RETURNING *";
         const res2 = await pool.query(suma, [monto, idReceptor]);
         if (res2) {
@@ -89,12 +76,11 @@ async function transferencias(emisor, receptor, monto, fechaHoraActual) {
         } else {
             console.error("Error al ejecutar la consulta SQL 2");
         }
-
+        //agregar transferencia
         const transferencia = "INSERT INTO transferencias (id, emisor, receptor, monto, fecha) VALUES(DEFAULT, $1, $2, $3, $4) RETURNING *";
         const res3 = await pool.query(transferencia, [idEmisor, idReceptor, monto, fechaHoraActual]);
         console.log("Operacion de transferencia exitosa: ", res3.rows[0]);
-
-        await pool.query("COMMIT"); //hay que verificar x cada res?
+        //validar que estén bien hechas las 3 consultas y commit si true, rollback si falla
         if (res1.rowCount == 1 && res2.rowCount == 1 && res3.rowCount == 1) {
             await pool.query("COMMIT");
             console.log("Transacción completa y exitosa");
@@ -112,17 +98,13 @@ async function transferencias(emisor, receptor, monto, fechaHoraActual) {
     }
 };
 
-// transferencias(95, 99, 2,"2024-05-08 15:30:00");
-
-// ● /transferencias GET: Devuelve todas las transferencias almacenadas en la base de datos en formato de arreglo. 
-
+//obtener transferencias registradas como array
 async function getTransferencias() {
     const result = await pool.query(
         {
             text: 'SELECT t.fecha, u.nombre, r.nombre, t.monto FROM transferencias t INNER JOIN usuarios u ON u.id = t.emisor INNER JOIN usuarios r ON r.id = t.receptor',
             rowMode: 'array'
         });
-    // console.log('usuarios en tabla transf:', result.rows)
     return result.rows;
 }
 
--- main.js
+++ main.js
@@ -1,121 +1,65 @@
-// npm init --yes
-// npm i express
-//npm i pg
-const {agregarUsuario,editarUsuario,todos,eliminarUsuario,transferencias,getTransferencias} = require('./consultas.js');
+//importe de funciones de consulta bd
+const { agregarUsuario, editarUsuario, todos, eliminarUsuario, transferencias, getTransferencias } = require('./consultas/consultas.js');
 
+//levantar servidor con express
 const express = require('express');
 const app = express();
 app.listen(3000, () => {
-     console.log('server en puerto 3mil')
+    console.log('server en puerto 3000')
 });
 
-
-//conectar bbdd postres con node
-//colsultas dml con node y pg
-//consultas TCL con node y pg
-//construir apirestful con posgres para persistencia datos
-//manejo errores y codigo de estado http
-
-//BANCO SOLAR Y SU NUEVO SISTEMA DE TRANSFERENCIAS
-//QUE REGISTRA NUEVOS USUARIOS CON UN BALANCE INICIAL Y BASADOS EN ESTOS, REALIZA ///////TRANSFERENCIAS//////// DE SALDOS ENTRE ELLOS
-// app.use(cors());
-// app.use(bodyParser.json()); se usaba antes cuando express no tenia esa funcuion (v4)
 // Middleware para parsear JSON osea que reconoce la instruccion de headers
-app.use(express.json()); 
-//solucion abran
-// app.use(express.text({ type: 'text/plain' })); //este para recibir texto plano del front(navegador)
+app.use(express.json());
 
-// un json es un texto plano
-
-// en este archivo principal van las rutas
-
-// ● / GET: Devuelve la aplicación cliente disponible en el apoyo de la prueba. 
-
+//sirviendo index
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
-//un servidor es una api sin front
-
-// ● /usuario POST: Recibe los datos de un nuevo usuario y los almacena en PostgreSQL.
-
-
-
+//agregar usuarios
 app.post('/usuario', async (req, res) => {
-
-
-    //NODE PURO 
-    // let body = '';
-    // req.on('data', (data) => body+=data)
-    // req.on('end', () => {
-    //     const data = JSON.parse(body)
-    //     console.log('req.on',data);
-    //     const nombre = data.nombre;
-    //     const balance = data.balance;
-    //     const result = agregarUsuario(nombre,balance)
-    //     console.log(result)
-    //     res.send(result);
-    // });
-
-    // "{"nombre":"juanito","balance":10000}"
-    // Obtener los datos del cuerpo de la solicitud
-    // const {nombre,balance} = JSON.parse(req.body); //parse lo convierte en objeto
-    const {nombre,balance} = req.body;
-    // const {nombre} = req.body.nombre;
-    // const {balance} = req.body.balance;
-    console.log(nombre,balance)
-    console.log('req.body',req.body);
-
+    const { nombre, balance } = req.body;
+    console.log(nombre, balance)
+    console.log('req.body', req.body);
     const result = await agregarUsuario(nombre, balance);
     console.log("Valor devuelto por la funcion de base de datos: ", result);
     res.status(200).send(result);
-
-    // Aquí deberías realizar la lógica para insertar el usuario en la base de datos
-    // Por ahora, simplemente imprimiremos los datos recibidos
-    // console.log(`Nuevo usuario: Nombre - ${nombre}, Balance - ${balance}`);
-
-    // Enviar una respuesta al cliente
-    // res.status(200).send('Usuario insertado correctamente');
 });
 
-// ● /usuarios GET: Devuelve todos los usuarios registrados con sus balances.
-
-app.get('/usuarios', async (req,res) => {
+// mostrar usuarios
+app.get('/usuarios', async (req, res) => {
     const result = await todos();
     console.log("Respuesta de la funcion todos: ", result);
     res.json(result);
 });
-// ● /usuario PUT: Recibe los datos modificados de un usuario registrado y los actualiza.
 
-app.put('/usuario', async (req,res) => {
-    const {id} = req.query;
-    const { name, balance } = req.body; //en la funcion del front se  llama name
-    const result = await editarUsuario(id,name,balance);
+// editar usuario
+app.put('/usuario', async (req, res) => {
+    const { id } = req.query;
+    const { name, balance } = req.body; 
+    const result = await editarUsuario(id, name, balance);
     console.log('valores editados:', result);
-    res.json(result); //POR QUE EN JSON?
+    res.json(result); 
 });
 
-// ● /usuario DELETE: Recibe el id de un usuario registrado y lo elimina .
-
-app.delete('/usuario', async (req,res) => {
-    const {id} = req.query;
+//borrar usuarios
+app.delete('/usuario', async (req, res) => {
+    const { id } = req.query;
     const resultado = await eliminarUsuario(id);
     console.log(resultado);
     res.json(resultado);
 });
 
-//  ● /transferencia POST: Recibe los datos para realizar una nueva transferencia.Se debe ocupar una transacción SQL en la consulta a la base de datos.
-
-app.post('/transferencia', async(req,res) => {
-    const {emisor,receptor,monto} = req.body;
-    console.log('recibido transferencia',req.body);
-    const resultado = await transferencias(emisor,receptor,monto);
+// transaccion trasferencia
+app.post('/transferencia', async (req, res) => {
+    const { emisor, receptor, monto } = req.body;
+    console.log('recibido transferencia', req.body);
+    const resultado = await transferencias(emisor, receptor, monto);
     res.json(resultado);
 });
 
-//   ● /transferencias GET: Devuelve todas las transferencias almacenadas en la base de datos en formato de arreglo. 
-
-app.get('/transferencias', async (req,res) => {
+// ver transferencias
+app.get('/transferencias', async (req, res) => {
     const result = await getTransferencias();
     console.log("Respuesta de la funcion getTrans: ", result);
     res.send(result);
